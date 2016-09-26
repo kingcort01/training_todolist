e4d91689600e8a636fa2118629d081bc1f21e92d
Caloy e ako <kingcort01.office@gmail.com>
2016-09-26 16:56:55 +0800

    initial project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
 import { Component } from '@angular/core';
+import { Todo } from './todo';
+
+const TODO = [
+  new Todo("Todo 1"),
+  new Todo("Todo 2"),
+  new Todo("Todo 3")
+];
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app works!';
+  title:string = 'Todo';
+  todolist : Array<Todo>
+  newTodo : string
+  editState : boolean
+
+  constructor(){
+    this.todolist = TODO
+    this.newTodo = ''
+    this.editState = false
+  }
+
+  deleteTodo(index){
+    this.todolist.splice(index, 1)
+  }
+
+  addTodo(event){
+    if(this.newTodo){
+      this.todolist.push(new Todo(this.newTodo))
+      this.newTodo = "";
+    }else{
+      alert("Please fill in the name!")
+    }
+  }
+
+  editTodo(index){
+    return 1
+  }
+
 }
